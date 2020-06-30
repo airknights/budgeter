efa53af30202f38811ec9e9ecde35b6e5ebd56ed
shasan <shasan@marsdd.com>
2020-06-30 10:02:10 -0400

    Initialized material design into the app.

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
+	import Button from '@smui/button';
 	export let name;
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
+<svelte:head>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono">
+</svelte:head>
 
-<h1>Hello {name}!</h1>
+<h1 class="mdc-typography--headline1">Hello {name}!</h1>
+<Button on:click={() => alert('clicked!')}>Just a Button</Button>
